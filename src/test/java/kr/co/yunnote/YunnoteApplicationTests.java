977b3744bbf04b2b2777e6fdf03c90246e782e38
YunJin Choi <zzdd1558@gmail.com>
2020-10-20 23:24:03 +0900

    #1 Initilize Project

---
+++ src/test/java/kr/co/yunnote/YunnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yunnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
